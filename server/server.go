2927583ff5c4f593b187f2a7696bd0c795a5decb
Vladislav Larkin <larkin.qubership@gmail.com>
2026-07-10 16:18:11 +0400

    chore: update github.com/gofiber/fiber/v3 to v3.4.0

--- server/server.go
+++ server/server.go
@@ -11,18 +11,18 @@ import (
 
 var logger = logging.GetLogger("server")
 
-func StartServer(app *fiber.App, listenAddressKey string) {
+func StartServer(app *fiber.App, listenAddressKey string, listenConfig ...fiber.ListenConfig) {
 	defaultListenAddress := ":8080"
 	if utils.IsTlsEnabled() {
 		defaultListenAddress = ":8443"
 	}
 	listenAddress := configloader.GetOrDefaultString(listenAddressKey, defaultListenAddress)
-	StartServerOnAddress(app, listenAddress)
+	StartServerOnAddress(app, listenAddress, listenConfig...)
 }
 
 func StartServerOnAddress(app *fiber.App, listenAddress string, listenConfig ...fiber.ListenConfig) {
 	if utils.IsTlsEnabled() {
-		ln, err := tls.Listen(fiber.NetworkTCP4, listenAddress, utils.GetTlsConfig())
+		ln, err := tls.Listen(listenerNetwork(listenConfig), listenAddress, utils.GetTlsConfig())
 		if err != nil {
 			logger.Panic("Cannot create listener on address=%s, error=%+v", listenAddress, err)
 		}
@@ -34,4 +34,14 @@ func StartServerOnAddress(app *fiber.App, listenAddress string, listenConfig ...
 			logger.Panic("Cannot start listener on address=%s, error=%+v", listenAddress, err)
 		}
 	}
+}
+
+// listenerNetwork returns the network for the TLS listener, honoring an explicit
+// ListenerNetwork from the caller's ListenConfig and defaulting to fiber's tcp4,
+// matching the non-TLS app.Listen default.
+func listenerNetwork(listenConfig []fiber.ListenConfig) string {
+	if len(listenConfig) > 0 && listenConfig[0].ListenerNetwork != "" {
+		return listenConfig[0].ListenerNetwork
+	}
+	return fiber.NetworkTCP4
 }
